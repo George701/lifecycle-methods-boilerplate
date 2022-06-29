2d44c12d860cf681453973283176d62761fa9279
George701 <chernikov.georgii@gmail.com>
2022-06-29 13:23:17 +0300

    Add state and render to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Post from './Components/Post';
+import EditPost from './Components/EditPost';
 
 export default class App extends Component {
-
+  state = {
+    creatingNewPost: false,
+    loadingPosts: true,
+    posts: []
+  };
+  
   render() {
+    const { posts, loadingPosts, creatingNewPost } = this.state;
     return (
-      <div>
+      <div className='wrapper'>
+        {
+          creatingNewPost
+          ? <EditPost closeModal={() => this.setState({ creatingNewPost: false})} editPost={this.editPost}/>
+          : (
+            <div className='new-post'>
+              <div className='btn' onClick={() => this.setState({ creatingNewPost: true})}>
+                New
+              </div>
+            </div>
+          )
+        }
+        { loadingPosts && <div>loading posts...</div> }
+        { (!loadingPosts && posts.length === 0) && <div>There is no posts yet</div> }
+        { (!loadingPosts  && posts.length !== 0)
+          && posts.map(post => (
+            <Post key={post.id} {...post} deletePost={() => this.deletePost(post.id)} editPost={this.editPost}/>)
+          )
+        }
       </div>
     )
   }
