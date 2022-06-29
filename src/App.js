64b7a54dd565aa77e2d99497f7cfeaed391d6927
George701 <chernikov.georgii@gmail.com>
2022-06-29 13:17:50 +0300

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { loadPosts, deletePostAPI, addPost, updatePost } from './services';
-import Post from './Components/Post';
-import EditPost from './Components/EditPost';
 
 export default class App extends Component {
 
